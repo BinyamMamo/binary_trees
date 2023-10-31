dbddaeab8e3764204f499e5960b17a162ed32ae4
BinyamMamo <binyammamo01@gmail.com>
2023-10-31 14:05:40 +0400

    Tue Oct 31 14:05:40 +04 2023

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,13 +12,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *tree_node;
 	
 	tree_node = malloc(sizeof(binary_tree_t));
-	if (tree_node != NULL)
-	{
-		tree_node->n = value;
-		tree_node->parent = parent;
-		tree_node->left = NULL;
-		tree_node->right = NULL;
-	}
+	if (tree_node == NULL)
+		return (NULL)
+
+	tree_node->n = value;
+	tree_node->parent = parent;
+	tree_node->left = NULL;
+	tree_node->right = NULL;
 
 	return (tree_node);
 }
