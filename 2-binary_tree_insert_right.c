999a1d0da57c01b48c02f11081b502794a1da293
BinyamMamo <binyammamo01@gmail.com>
2023-10-31 14:35:31 +0400

    Tue Oct 31 14:35:31 +04 2023

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,13 +2,13 @@
 
 
 /**
- * binary_tree_insert_left - Inserts a new node to the left of the parent
+ * binary_tree_insert_right - Inserts a new node to the right of the parent
  * @parent: The parent of the new node
  * @value: The value to be inserted
  *
  * Return: The new node or NULL
 */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *tree_node = NULL;
 
@@ -17,13 +17,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (tree_node == NULL)
 		return (NULL);
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
-		tree_node->left = parent->left;
-		tree_node->left->parent = tree_node;
+		tree_node->right = parent->right;
+		tree_node->right->parent = tree_node;
 	}
 
-	parent->left = tree_node;
+	parent->right = tree_node;
 
 	return (tree_node);
 }
